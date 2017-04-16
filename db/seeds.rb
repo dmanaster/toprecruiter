6ec2aba0a07dba835bdefd3529116879fa269c3a
dmanaster <david@manaster.com>
2017-04-15 20:57:57 -0700

    Updates seed file to use a find_or_create_by method.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,4 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
-episodes = Episode.create([
+hashes = [
   {
     title: "Episode 1 - Youtube",
     url: "https://www.youtube.com/watch?v=2YzM4ZXnJvU",
@@ -18,5 +10,8 @@ episodes = Episode.create([
     url: "https://www.facebook.com/toprecruitertv/videos/vb.195576537163412/1307086426012412/?type=2&theater",
     published: "Apr 10, 2017".to_date,
     source: "facebook"
-  }
-  ])
+  }]
+
+hashes.each do |hash|
+  Episode.find_or_create_by(hash)
+end
